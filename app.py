76e7f3e529d60a58f1907024477987f48a64220c
Tia Hannah <65368702+tiakorre@users.noreply.github.com>
2024-01-01 21:06:04 +0000

    skeloton

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['your_database_name']
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
